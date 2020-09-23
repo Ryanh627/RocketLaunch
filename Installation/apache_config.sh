08e13035e31f44ee0da8da0ce2abcdb28d0ac51f
eckertb16 <ben.eckert16@gmail.com>
2020-09-23 00:57:58 -0400

    Added installation directory and apache_config.sh batch file

---
+++ Installation/apache_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install apache2
+sudo apt-get install apache2-dev
+sudo apt install libapache2-mod-wsgi-py3 -y
+sudo mv ../Dependencies/rocketlaunch.conf /etc/apache2/sites-available
+sudo a2ensite rocketlaunch.conf
+sudo a2dissite 000-default.conf
+sudo service apache2 restart
