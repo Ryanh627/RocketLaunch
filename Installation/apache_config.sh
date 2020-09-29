d37165d2eafa214e2d41e5150274b62e70175f94
eckertb16 <ben.eckert16@gmail.com>
2020-09-29 18:40:55 -0400

    Changes to batch file

--- Installation/apache_config.sh
+++ Installation/apache_config.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+sudo apt-get update
 sudo apt-get install apache2
 sudo apt-get install apache2-dev
 sudo apt install libapache2-mod-wsgi-py3 -y
-sudo mv ../Dependencies/rocketlaunch.conf /etc/apache2/sites-available
+sudo cp ../Dependencies/rocketlaunch.conf /etc/apache2/sites-available
 sudo a2ensite rocketlaunch.conf
 sudo a2dissite 000-default.conf
 sudo service apache2 restart
