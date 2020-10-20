f0f9868ae3251849d3bcbc70c26237af8d028ea0
Awkemacspjg <awkemacspjg2@gmail.com>
2020-10-20 11:10:58 -0400

    Fixed launch.py

--- Source/WebApp/launch.py
+++ Source/WebApp/launch.py
@@ -1,10 +1,10 @@
 
 
 import RPi.GPIO as GPIO
+from pad import *
 GPIO.setmode(GPIO.BOARD)
-pad = ""
 
-if (pad. == "1"){
+if (pad.pinIn == 13){
 GPIO.setup(37,GPIO.OUT) 
 GPIO.output(37,GPIO.HIGH) 
 GPIO.output(37,GPIO.LOW)
@@ -12,7 +12,7 @@ time.sleep(1)
 GPIO.output(37,GPIO.HIGH) 
 print "Launched Pad1"
 }
-if (pad == "2"){
+if (pad.pinIn == 15){
 GPIO.setup(38,GPIO.OUT) 
 GPIO.output(38,GPIO.HIGH) 
 GPIO.output(38,GPIO.LOW)
@@ -20,7 +20,7 @@ time.sleep(1)
 GPIO.output(38,GPIO.HIGH) 
 print "Launched Pad2"}
 
-if (pad == "3"){
+if (pad.pinIn == 23){
 GPIO.setup(40,GPIO.OUT) 
 GPIO.output(40,GPIO.HIGH) 
 GPIO.output(40,GPIO.LOW)
