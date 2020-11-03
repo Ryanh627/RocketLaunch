f211c2eb8890f70c5e6e0974b7921f44da69ddd8
Ryan627 <Ryanh1mail@gmail.com>
2020-11-03 14:47:33 -0500

    config file for launch settings and config parser stuff

--- Source/WebApp/Camera.py
+++ Source/WebApp/Camera.py
@@ -1,13 +1,20 @@
 import picamera
+import configparser
 from time import sleep
 
 def take_video():
-    camera = picamera.PiCamera()
     
-    location = "/home/pi/RocketLaunch/Source/WebApp/static/media/Clips"
-    videoname = "clip{counter:02d}.h264"
-    duration = 5
-    camera.start_recording(location+videoname)
-    camera.wait_recording(duration)
-    camera.stop_recording()
-    camera.close()
+    config = configparser.ConfigParser()
+    config.read('launch_settings.conf')
+    
+    if config.getboolean('doVideo')
+    
+        camera = picamera.PiCamera()
+    
+        location = "/home/pi/RocketLaunch/Source/WebApp/static/media/Clips"
+        videoname = "clip{counter:02d}.h264"
+        duration = config.getint('duration')
+        camera.start_recording(location+videoname)
+        camera.wait_recording(duration)
+        camera.stop_recording()
+        camera.close()
