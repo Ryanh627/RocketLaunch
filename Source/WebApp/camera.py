32fbc112a215e9b5fe925621543af4cac6eda194
Ryanh627 <ryanh1mail@gmail.com>
2020-11-25 18:13:38 -0500

    video converts to mp4

--- Source/WebApp/camera.py
+++ Source/WebApp/camera.py
@@ -3,22 +3,31 @@
 #camera.py controls the functionality of the camera wired to the pi
 
 import picamera
-import configparser
-from time import sleep
+from database import *
+from subprocess import call
+from datetime import date
+import time
 
 def take_video():
     
-    config = configparser.ConfigParser()
-    config.read('launch_settings.conf')
-    
-    if config.getboolean('doVideo'):
+    if db_get_setting("RECORDLAUNCH"):
         
         camera = picamera.PiCamera()
-        location = "/home/pi/RocketLaunch/Source/WebApp/static/media/Clips"
-        videoname = "clip{counter:02d}.h264"
-        duration = config.getint('duration')
+        location = "/home/pi/RocketLaunch/Source/WebApp/static/media/Clips/"
+        videoname = "tempclip.h264"
+        File_h264 = location+videoname
+        date = fdate = date.today().strftime('%d/%m/%Y')
+        time = time.strftime("%H:%M:%S")
+        
+        command = "MP4Box -add " + location+videoname + " " + location + "clip"+date+ ":" + time + ".mp4"
+        
+        duration = db_get_setting("RECORDINGDURATION")
         
         camera.start_recording(location+videoname)
         camera.wait_recording(duration)
         camera.stop_recording()
         camera.close()
+        
+        call([command], shell = True)
+        
+        #db_insert_video(users, location + "clip.mp4")
